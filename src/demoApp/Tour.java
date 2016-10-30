1a1098d463116bb85c9b782ae1a6ab614cde746c
Jeffrey-Young <youngjt@live.unc.edu>
2016-10-29 22:09:05 -0400

    added tour class

---
+++ src/demoApp/Tour.java
@@ -0,0 +1,29 @@
+package demoApp;
+
+import java.util.ArrayList;
+
+import com.esri.arcgisruntime.geometry.Point;
+
+public class Tour {
+
+  private ArrayList<Point> _checkpoints;
+  private String           _name;
+
+  public Tour(String name) {
+    _checkpoints = new ArrayList<Point>();
+    _name = name;
+  }
+
+  public void addCheckpotint(double latitude, double longitude) {
+    _checkpoints.add(new Point(longitude, latitude));
+  }
+
+  public String getName() {
+    return _name;
+  }
+
+  public ArrayList<Point> getCheckPoints() {
+    return _checkpoints;
+  }
+
+}
